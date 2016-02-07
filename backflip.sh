a2fb7fbe5ee2f43f29231d1009ba657d271d6f72
Ben Davenport-Ray <ben.davenportray@gmail.com>
2016-02-07 13:24:42 -0500

    Adding features to backflip. Still a lot of bugs.

--- backflip.sh
+++ backflip.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 #
-# z3bra - 2014 (c) wtfpl
-# focus a window when it is created
-# depends on: wew focus.sh
+# Do a backflip.
 
+rainbow.sh &
+last_window=
 wew | while IFS=: read ev wid; do
     case $ev in
     # Window focusing
-    7) focus.sh $wid ;;
+    7) 
+        focus.sh $wid
+        ;;
     # occurs on mapped windows
     16) 
-        wattr o $wid || focus.sh $wid 
+        if [[ ! $(wattr o $wid) ]]; then
+            focus.sh $wid
+        fi
         ;;
     esac
 done
---
+++ tray.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+trayer --edge top --align right --widthtype request --heighttype request --SetDockType true \
+    --transparent true --alpha 255 &
